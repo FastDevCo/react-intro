1a1b7780e8070d0823d8e66486ada5eda6beb3ae
Elias Nygren <elias.nygren@outlook.com>
2017-04-21 21:54:43 +0300

    convert .createClass into a pure component

--- example_solution/components/todoheader.js
+++ example_solution/components/todoheader.js
@@ -14,27 +14,24 @@ import React, {Component} from 'react';
 
 */
 
-export const TodoProgressBar = React.createClass({
-  render: function () {
+export const TodoProgressBar = (props) => {
+  // Calculate statistics
+  const stats = props.tasks.reduce((stats, task) => {
+    return {
+      done: task.done ? stats.done + 1 : stats.done,
+      undone: !task.done ? stats.undone + 1 : stats.undone,
+      total: stats.total + 1
+    };
+  }, {done: 0, undone: 0, total: 0});
 
-    // Calculate statistics
-    const stats = this.props.tasks.reduce((stats, task) => {
-      return {
-        done: task.done ? stats.done + 1 : stats.done,
-        undone: !task.done ? stats.undone + 1 : stats.undone,
-        total: stats.total + 1
-      };
-    }, {done: 0, undone: 0, total: 0});
+  // Calculate progress-bar width
+  const style = {
+    width: `${stats.done / stats.total * 100}%`
+  };
 
-    // Calculate progress-bar width
-    const style = {
-      width: stats.done / stats.total * 100 + "%"
-    }
-
-    return (
-      <div className="progress-container">
-        <div className="progress" style={ style }>&nbsp;</div>
-      </div>
-    );
-  }
-});
+  return (
+    <div className='progress-container'>
+      <div className='progress' style={style}>&nbsp;</div>
+    </div>
+  );
+}
