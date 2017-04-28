6414d9c1423b747d31336f3b9b91da785cd6f2a8
Teemu Heikkilä <teemu@emblica.fi>
2017-04-28 10:48:27 +0300

    Fix to use local storage model

--- example_solution/index.js
+++ example_solution/index.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 // this our data store abstraction
-// import {TodoData} from './model/todomodel';
+import {TodoData} from './model/todomodel';
 // use this instead if you want to test this with https://github.com/FastDevCo/nodejs-intro
-import {TodoData} from './model/todo_apimodel';
+//import {TodoData} from './model/todo_apimodel';
 
 // note: these are regular React components too !
 import {TodoProgressBar} from './components/todoheader';
